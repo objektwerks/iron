fa756701eb01d66680614cd6750042d6cf681726
objektwerks <objektwerks@runbox.com>
2023-08-28 12:21:27 -0400

    added type of chemical

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -7,6 +7,16 @@ import io.github.iltotore.iron.constraint.numeric.{Greater, GreaterEqual}
 enum UnitOfMeasure:
   case gl, l, lb, kg, tablet
 
+enum TypeOfChemical(val display: String):
+  case LiquidChlorine extends TypeOfChemical("Liquid Chlorine")
+  case Trichlor extends TypeOfChemical("Trichlor")
+  case Dichlor extends TypeOfChemical("Dichlor")
+  case CalciumHypochlorite extends TypeOfChemical("Calcium Hypochlorite")
+  case Stabilizer extends TypeOfChemical("Stabilizer")
+  case Algaecide extends TypeOfChemical("Algaecide")
+  case MuriaticAcid extends TypeOfChemical("Muriatic Acid")
+  case Salt extends TypeOfChemical("Salt")
+
 sealed trait Entity:
   val id: Long :| GreaterEqual[0]
 
