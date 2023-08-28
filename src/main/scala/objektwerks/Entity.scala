d8724fff1bd87b8db49679afce05f3118dae3764
objektwerks <objektwerks@runbox.com>
2023-08-28 12:09:40 -0400

    added pool

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -1,7 +1,14 @@
 package objektwerks
 
 import io.github.iltotore.iron.*
-import io.github.iltotore.iron.constraint.numeric.GreaterEqual
+import io.github.iltotore.iron.constraint.numeric.{Greater, GreaterEqual}
+import io.github.iltotore.iron.constraint.string.Alphanumeric
 
 sealed trait Entity:
   val id: Long :| GreaterEqual[0]
+
+final case class Pool(id: Long :| GreaterEqual[0],
+                      license: String :| Alphanumeric,
+                      name: String :| Alphanumeric, 
+                      volume: Int :| Greater[100],
+                      unit: String :| Alphanumeric) extends Entity
