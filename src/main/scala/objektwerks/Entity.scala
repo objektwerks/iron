5b9d947973917668659dcc9ab789902252420e1e
objektwerks <objektwerks@runbox.com>
2023-08-28 11:40:27 -0400

    enhanced entity

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -4,4 +4,4 @@ import io.github.iltotore.iron.*
 import io.github.iltotore.iron.constraint.numeric.Greater
 
 sealed trait Entity:
-  val id: Long :| Greater[-1]
+  val id: Long :| Greater[-1] DescribedAs "Must be 0 or greater."
