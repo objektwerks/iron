dc0e92a261780dde6ba4a38f2fba6073de99c13c
objektwerks <objektwerks@runbox.com>
2023-08-29 07:27:54 -0400

    edited account

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -21,7 +21,7 @@ object Entity:
   given JsonValueCodec[Measurement] = JsonCodecMaker.make[Measurement]( CodecMakerConfig.withDiscriminatorFieldName(None) )
   given JsonValueCodec[Chemical] = JsonCodecMaker.make[Chemical]( CodecMakerConfig.withDiscriminatorFieldName(None) )
 
-final case class Account(id: Long :| GreaterEqual[0],
+final case class Account(id: Long :| GreaterEqual[1],
                          license: String :| ValidUUID,
                          emailAddress: String :| MinLength[3],
                          pin: String :| FixedLength[7],
