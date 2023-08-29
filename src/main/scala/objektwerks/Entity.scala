2b68a5caefe4b9a7c03e8d7e94cb3464a3e11bce
objektwerks <objektwerks@runbox.com>
2023-08-29 07:30:37 -0400

    edited cleaning

--- src/main/scala/objektwerks/Entity.scala
+++ src/main/scala/objektwerks/Entity.scala
@@ -35,7 +35,7 @@ final case class Pool(id: Long :| Greater[0],
                       volume: Int :| GreaterEqual[100],
                       unit: UnitOfMeasure) extends Entity
 
-final case class Cleaning(id: Long :| GreaterEqual[1],
+final case class Cleaning(id: Long :| Greater[0],
                           poolId: Long :| GreaterEqual[1],
                           brush: Boolean,
                           net: Boolean,
