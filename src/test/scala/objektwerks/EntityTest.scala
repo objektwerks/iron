03e23a0fd2aa4507622f3760db50d101027b2bcf
objektwerks <objektwerks@runbox.com>
2023-08-30 07:19:49 -0400

    added entity test

---
+++ src/test/scala/objektwerks/EntityTest.scala
@@ -0,0 +1,9 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class EntityTest extends AnyFunSuite with Matchers:
+  test("entity") {
+    
+  }
