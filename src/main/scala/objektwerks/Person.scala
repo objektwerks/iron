278862815e8384abfabdd55f234c512fa700a03c
objektwerks <objektwerks@runbox.com>
2023-10-27 07:31:12 -0400

    corrected json issue with person

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -4,23 +4,22 @@ import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.github.plokhotnyuk.jsoniter_scala.macros.*
 
 import io.github.iltotore.iron.*
-import io.github.iltotore.iron.jsoniter.given
 import io.github.iltotore.iron.constraint.collection.{FixedLength, MinLength}
 import io.github.iltotore.iron.constraint.numeric.Greater
 
 import scala.collection.mutable
 
-sealed abstract case class Person private(id: String, name: String, age: Int)
+final case class Person private(id: String, name: String, age: Int)
 
 object Person:
-  given JsonValueCodec[Person] = JsonCodecMaker.make[Person] // Results in stackoverflow error!
+  given JsonValueCodec[Person] = JsonCodecMaker.make[Person]
 
   def validate(id: String, name: String, age: Int): Either[String, Person] =
     for
       i <- id.refineEither[FixedLength[1]]
       n <- name.refineEither[MinLength[2]]
       a <- age.refineEither[Greater[0]]
-    yield new Person(i, n, a) {}
+    yield Person(i, n, a)
 
   def validations(id: String, name: String, age: Int): Map[String, String] =
     val map = mutable.Map.empty[String, String]
