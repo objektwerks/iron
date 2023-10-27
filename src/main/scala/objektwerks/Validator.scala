f6c033f183d5f94acc6dec6cc7267fbcf247dd79
objektwerks <objektwerks@runbox.com>
2023-10-27 05:38:02 -0400

    edited validator

--- src/main/scala/objektwerks/Validator.scala
+++ src/main/scala/objektwerks/Validator.scala
@@ -11,9 +11,9 @@ object Validator:
     def validate: Map[String, String] =
       val map = mutable.Map.empty[String, String]
       account.id.refineEither[GreaterEqual[0]].left.map(error => map += "id" -> error)
-      //account.license.refineEither[FixedLength[36]].left.map(error => map += "id" -> error)
-      //account.emailAddress.refineEither[MinLength[3]].left.map(error => map += "id" -> error)
-      //account.pin.refineEither[FixedLength[7]].left.map(error => map += "id" -> error)
-      account.activated.refineEither[GreaterEqual[0]].left.map(error => map += "id" -> error)
-      account.deactivated.refineEither[GreaterEqual[0]].left.map(error => map += "id" -> error)
+      //account.license.refineEither[FixedLength[36]].left.map(error => map += "license" -> error)
+      //account.emailAddress.refineEither[MinLength[3]].left.map(error => map += "emailAddress" -> error)
+      //account.pin.refineEither[FixedLength[7]].left.map(error => map += "pin" -> error)
+      account.activated.refineEither[GreaterEqual[0]].left.map(error => map += "activated" -> error)
+      account.deactivated.refineEither[GreaterEqual[0]].left.map(error => map += "deactivated" -> error)
       map.toMap
