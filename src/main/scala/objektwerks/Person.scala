827fba9e5743affecd1f53bacd720d1b5e11b470
objektwerks <objektwerks@runbox.com>
2023-10-27 12:11:45 -0400

    refactored person

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -4,19 +4,18 @@ import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.github.plokhotnyuk.jsoniter_scala.macros.*
 
 import io.github.iltotore.iron.*
-import io.github.iltotore.iron.constraint.collection.{FixedLength, MinLength}
+import io.github.iltotore.iron.constraint.collection.MinLength
 import io.github.iltotore.iron.constraint.numeric.Greater
 
-final case class Person private(id: String, name: String, age: Int)
+final case class Person private(name: String, age: Int)
 
 object Person:
   given JsonValueCodec[Person] = JsonCodecMaker.make[Person]
 
-  def validate(id: String, name: String, age: Int): Validated[Person] =
+  def validate(name: String, age: Int): Validated[Person] =
     val validations = Validations()
     val either = for
-      i <- id.refineEither[FixedLength[1]].left.map(error => validations.add("id", error))
       n <- name.refineEither[MinLength[2]].left.map(error => validations.add("name", error))
       a <- age.refineEither[Greater[0]].left.map(error => validations.add("age", error))
-    yield Person(i, n, a)
+    yield Person(n, a)
     Validated(validations, either.right.get)
