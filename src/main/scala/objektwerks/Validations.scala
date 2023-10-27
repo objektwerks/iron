e6c0a5b791bdb6671084739f99d7f40e84e8c478
objektwerks <objektwerks@runbox.com>
2023-10-27 11:35:51 -0400

    added validations

---
+++ src/main/scala/objektwerks/Validations.scala
@@ -0,0 +1,17 @@
+package objektwerks
+
+import scala.collection.mutable
+
+final class Validations:
+  type Field = String
+  type Message = String
+
+  private val invalidFields = mutable.Map[Field, Message]()
+
+  def isValid: Boolean = invalidFields.isEmpty
+
+  def add(field: Field, message: Message): Unit = invalidFields += field -> message
+
+  def get(field: Field): Option[Message] = invalidFields.get(field)
+
+  def toMap: Map[Field, Message] = invalidFields.toMap
