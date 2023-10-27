c5785f3340929bbd786b67037142cc9f93e82171
objektwerks <objektwerks@runbox.com>
2023-10-27 07:36:02 -0400

    enhanced person test

--- src/test/scala/objektwerks/PersonTest.scala
+++ src/test/scala/objektwerks/PersonTest.scala
@@ -5,5 +5,8 @@ import org.scalatest.matchers.should.Matchers
 
 final class PersonTest extends AnyFunSuite with Matchers:
   test("person"):
-    val validatedPerson = Person.validate("1", "fred", 24)
-    validatedPerson.isRight shouldBe true
+    val validPerson = Person.validate("1", "fred", 24)
+    validPerson.isRight shouldBe true
+
+    val invalidPerson = Person.validate("12", "fred", 24)
+    invalidPerson.isLeft shouldBe true
