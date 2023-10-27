d39b02bbbe06a36f65c482ae3092a81ea7e4580a
objektwerks <objektwerks@runbox.com>
2023-10-27 06:52:04 -0400

    enhanced person

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -4,6 +4,8 @@ import io.github.iltotore.iron.*
 import io.github.iltotore.iron.constraint.collection.MinLength
 import io.github.iltotore.iron.constraint.numeric.Greater
 
+import scala.collection.mutable
+
 sealed abstract case class Person private(name: String, age: Int)
 
 object Person:
@@ -12,3 +14,11 @@ object Person:
       n <- name.refineEither[MinLength[2]]
       a <- age.refineEither[Greater[0]]
     yield new Person(n, a) {}
+
+  def validations(name: String, age: Int): Map[String, String] =
+    val map = mutable.Map.empty[String, String]
+    for
+      _ <- name.refineEither[MinLength[2]].left.map(error => map += "name" -> error)
+      _ <- age.refineEither[Greater[0]].left.map(error => map += "age" -> error)
+    yield ()
+    map.toMap
