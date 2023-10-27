b451e8eb063a344c2b9b6d8c95ef5b1794beac7f
objektwerks <objektwerks@runbox.com>
2023-10-27 06:55:01 -0400

    enhanced validator

--- src/main/scala/objektwerks/Validator.scala
+++ src/main/scala/objektwerks/Validator.scala
@@ -1,7 +1,7 @@
 package objektwerks
 
 import io.github.iltotore.iron.*
-//import io.github.iltotore.iron.constraint.collection.{FixedLength, MinLength}
+import io.github.iltotore.iron.constraint.collection.{FixedLength, MinLength}
 import io.github.iltotore.iron.constraint.numeric.{Greater, GreaterEqual}
 
 import scala.collection.mutable
@@ -10,10 +10,12 @@ object Validator:
   extension(account: Account)
     def validate(): Map[String, String] =
       val map = mutable.Map.empty[String, String]
-      account.id.refineEither[GreaterEqual[0]].left.map(error => map += "id" -> error)
-      //account.license.refineEither[FixedLength[36]].left.map(error => map += "license" -> error)
-      //account.emailAddress.refineEither[MinLength[3]].left.map(error => map += "emailAddress" -> error)
-      //account.pin.refineEither[FixedLength[7]].left.map(error => map += "pin" -> error)
-      account.activated.refineEither[GreaterEqual[0]].left.map(error => map += "activated" -> error)
-      account.deactivated.refineEither[GreaterEqual[0]].left.map(error => map += "deactivated" -> error)
+      for
+        _ <- account.id.refineEither[GreaterEqual[0]].left.map(error => map += "id" -> error)
+        _ <- account.license.refineEither[FixedLength[36]].left.map(error => map += "license" -> error)
+        _ <- account.emailAddress.refineEither[MinLength[3]].left.map(error => map += "emailAddress" -> error)
+        _ <- account.pin.refineEither[FixedLength[7]].left.map(error => map += "pin" -> error)
+        _ <- account.activated.refineEither[GreaterEqual[0]].left.map(error => map += "activated" -> error)
+        _ <- account.deactivated.refineEither[GreaterEqual[0]].left.map(error => map += "deactivated" -> error)
+      yield ()
       map.toMap
