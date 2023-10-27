0455aa087aadb333696805c2ebcd585683958f7a
objektwerks <objektwerks@runbox.com>
2023-10-27 07:11:00 -0400

    enhanced person

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -1,5 +1,8 @@
 package objektwerks
 
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+import com.github.plokhotnyuk.jsoniter_scala.macros.*
+
 import io.github.iltotore.iron.*
 import io.github.iltotore.iron.constraint.collection.{FixedLength, MinLength}
 import io.github.iltotore.iron.constraint.numeric.Greater
@@ -9,6 +12,8 @@ import scala.collection.mutable
 sealed abstract case class Person private(id: String, name: String, age: Int)
 
 object Person:
+  given JsonValueCodec[Person] = JsonCodecMaker.make[Person]
+
   def validate(id: String, name: String, age: Int): Either[String, Person] =
     for
       i <- id.refineEither[FixedLength[1]]
