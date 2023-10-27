de80f1a0eefe5e934de1b3f68ba19480df4a7a16
objektwerks <objektwerks@runbox.com>
2023-10-27 11:00:13 -0400

    enhanced person

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -14,20 +14,13 @@ final case class Person private(id: String, name: String, age: Int)
 object Person:
   given JsonValueCodec[Person] = JsonCodecMaker.make[Person]
 
-  def validate(id: String, name: String, age: Int): Either[String, Person] =
-    for
-      i <- id.refineEither[FixedLength[1]]
-      n <- name.refineEither[MinLength[2]]
-      a <- age.refineEither[Greater[0]]
-    yield Person(i, n, a)
-
-  def validations(id: String, name: String, age: Int): Either[Map[String, String], Person] =
+  def validate(id: String, name: String, age: Int): Either[Map[String, String], Person] =
     val map = mutable.Map.empty[String, String]
-    val person = for
+    val either = for
       i <- id.refineEither[FixedLength[1]].left.map(error => map += "name" -> error)
       n <- name.refineEither[MinLength[2]].left.map(error => map += "name" -> error)
       a <- age.refineEither[Greater[0]].left.map(error => map += "age" -> error)
     yield Person(i, n, a)
 
-    if map.isEmpty then Right(person.right.get)
+    if map.isEmpty then Right(either.right.get)
     else Left(map.toMap)
