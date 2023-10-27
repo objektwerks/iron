2a786fe28061a15acd5d84ff82e0d990a6fcd0d6
objektwerks <objektwerks@runbox.com>
2023-10-27 10:58:13 -0400

    enhanced person

--- src/main/scala/objektwerks/Person.scala
+++ src/main/scala/objektwerks/Person.scala
@@ -21,11 +21,13 @@ object Person:
       a <- age.refineEither[Greater[0]]
     yield Person(i, n, a)
 
-  def validations(id: String, name: String, age: Int): Map[String, String] =
+  def validations(id: String, name: String, age: Int): Either[Map[String, String], Person] =
     val map = mutable.Map.empty[String, String]
-    for
-      _ <- id.refineEither[FixedLength[1]].left.map(error => map += "name" -> error)
-      _ <- name.refineEither[MinLength[2]].left.map(error => map += "name" -> error)
-      _ <- age.refineEither[Greater[0]].left.map(error => map += "age" -> error)
-    yield ()
-    map.toMap
+    val person = for
+      i <- id.refineEither[FixedLength[1]].left.map(error => map += "name" -> error)
+      n <- name.refineEither[MinLength[2]].left.map(error => map += "name" -> error)
+      a <- age.refineEither[Greater[0]].left.map(error => map += "age" -> error)
+    yield Person(i, n, a)
+
+    if map.isEmpty then Right(person.right.get)
+    else Left(map.toMap)
