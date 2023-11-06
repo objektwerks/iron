aaac36f46a1d57286dbb8d4f4b8b0d60722c2791
objektwerks <objektwerks@runbox.com>
2023-11-06 08:10:59 -0500

    enhanced event

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -1,5 +1,11 @@
 package objektwerks
 
+import com.github.plokhotnyuk.jsoniter_scala.core.*
+import com.github.plokhotnyuk.jsoniter_scala.macros.*
+
 sealed trait Event
 
 final case class PersonAdded(person: Either[Invalidations, Person]) extends Event
+
+object Event:
+  given JsonValueCodec[PersonAdded] = JsonCodecMaker.make[PersonAdded]
