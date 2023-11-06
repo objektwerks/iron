bb41f7796a2431571ddcba568a855c38bafd4975
objektwerks <objektwerks@runbox.com>
2023-11-06 08:18:24 -0500

    enhanced event

--- src/main/scala/objektwerks/Event.scala
+++ src/main/scala/objektwerks/Event.scala
@@ -11,10 +11,11 @@ final case class AccountAdded(account: Either[Invalidations, Account]) extends E
 final case class PoolAdded(pool: Either[Invalidations, Pool]) extends Event
 final case class CleaningAdded(pool: Either[Invalidations, Cleaning]) extends Event
 final case class MeasurementAdded(pool: Either[Invalidations, Measurement]) extends Event
+final case class ChemicalAdded(pool: Either[Invalidations, Chemical]) extends Event
 
 object Event:
   given JsonValueCodec[Event] = JsonCodecMaker.make[Event]
   given JsonValueCodec[PersonAdded] = JsonCodecMaker.make[PersonAdded]
   given JsonValueCodec[PoolAdded] = JsonCodecMaker.make[PoolAdded]
   given JsonValueCodec[CleaningAdded] = JsonCodecMaker.make[CleaningAdded]
-  given JsonValueCodec[MeasurementAdded] = JsonCodecMaker.make[MeasurementAdded]
+  given JsonValueCodec[ChemicalAdded] = JsonCodecMaker.make[ChemicalAdded]
